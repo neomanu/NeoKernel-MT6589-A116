793eb46005c7f5da634524ee14d810d153d95f3d
Steven Rostedt (Red Hat) <rostedt@goodmis.org>
2014-06-24 23:50:09 -0400

    tracing: Remove ftrace_stop/start() from reading the trace file
    commit 099ed151675cd1d2dbeae1dac697975f6a68716d upstream.
    
    Disabling reading and writing to the trace file should not be able to
    disable all function tracing callbacks. There's other users today
    (like kprobes and perf). Reading a trace file should not stop those
    from happening.
    
    Reviewed-by: Masami Hiramatsu <masami.hiramatsu.pt@hitachi.com>
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -1092,7 +1092,6 @@ void tracing_start(void)
 
 	arch_spin_unlock(&ftrace_max_lock);
 
-	ftrace_start();
  out:
 	raw_spin_unlock_irqrestore(&tracing_start_lock, flags);
 }
@@ -1107,8 +1106,6 @@ void tracing_stop(void)
 {
 	struct ring_buffer *buffer;
 	unsigned long flags;
-
-	ftrace_stop();
 
 	raw_spin_lock_irqsave(&tracing_start_lock, flags);
 	if (trace_stop_count++)
