8369faea68fc85ca99ea5d28e160093e53d5dcdf
neomanu <neuvine@gmail.com>
2014-07-02 17:29:14 +0530

    Revert 'block: account iowait time when waiting for completion of IO request'

--- block/blk-lib.c
+++ block/blk-lib.c
@@ -113,7 +113,7 @@ int blkdev_issue_discard(struct block_device *bdev, sector_t sector,
 
 	/* Wait for bios in-flight */
 	if (!atomic_dec_and_test(&bb.done))
-		wait_for_completion_io(&wait);
+		wait_for_completion(&wait);
 
 	if (!test_bit(BIO_UPTODATE, &bb.flags))
 		ret = -EIO;
@@ -175,7 +175,7 @@ int blkdev_issue_zeroout(struct block_device *bdev, sector_t sector,
 
 	/* Wait for bios in-flight */
 	if (!atomic_dec_and_test(&bb.done))
-		wait_for_completion_io(&wait);
+		wait_for_completion(&wait);
 
 	if (!test_bit(BIO_UPTODATE, &bb.flags))
 		/* One of bios in the batch was completed with error.*/
